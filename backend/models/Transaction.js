633289179586e4904ba23153aaf4a58f06ceeadb
Mohit-Y-Kumar <my1362321@gmail.com>
2025-09-10 16:20:19 +0530

    Add Transaction.js model  in backend

---
+++ backend/models/Transaction.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true },
+  amount: { type: Number, required: true }, // positive = income, negative = expense
+  date: { type: Date, required: true },
+  category: { type: String, required: true, trim: true }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
