5cc3530dab9a811399ab89006893eb34c9a27e85
Mohit-Y-Kumar <my1362321@gmail.com>
2025-09-10 17:33:36 +0530

    update edit page

--- frontend/src/pages/EditTransaction.jsx
+++ frontend/src/pages/EditTransaction.jsx
@@ -1,12 +1,18 @@
 import { useParams } from "react-router-dom";
 import TransactionForm from "../components/TransactionForm";
+import { Pencil } from "lucide-react";
+
 
 const EditTransaction = () => {
   const { id } = useParams();
 
   return (
     <div className="p-6">
-      <h2 className="text-xl font-bold mb-4">✏️ Edit Transaction</h2>
+      <h2 className="text-2xl sm:text-3xl font-extrabold text-center text-gray-800 mb-6 flex items-center justify-center gap-2">
+  <Pencil className="w-6 h-6 text-blue-500" />
+  Edit Transaction
+</h2>
+
       <TransactionForm transactionId={id} />
     </div>
   );
