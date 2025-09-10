cd15ca8c8d000fcdc15ed46793f1deb6bc293eee
Mohit-Y-Kumar <my1362321@gmail.com>
2025-09-10 16:44:11 +0530

    Add transaction.jsx in frontend

---
+++ frontend/src/pages/AddTransaction.jsx
@@ -0,0 +1,12 @@
+import TransactionForm from "../components/TransactionForm";
+
+const AddTransaction = () => {
+  return (
+    <div className="p-6">
+      <h2 className="text-xl font-bold mb-4">➕ Add New Transaction</h2>
+      <TransactionForm />
+    </div>
+  );
+};
+
+export default AddTransaction;
