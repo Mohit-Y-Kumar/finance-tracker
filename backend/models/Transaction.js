0f9ef3ccfbfb8c8e7d2dd2aea2534a3fe51d041f
Mohit-Y-Kumar <my1362321@gmail.com>
2025-09-13 15:26:53 +0530

    Add user in transaction

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true },
-  amount: { type: Number, required: true }, // positive = income, negative = expense
+  amount: { type: Number, required: true }, 
   date: { type: Date, required: true },
   category: { type: String, required: true, trim: true },
    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" ,required:true},
