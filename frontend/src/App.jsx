bc98510cc25d5a414b83b181d6d7c1220a392c51
Mohit-Y-Kumar <my1362321@gmail.com>
2025-09-13 15:23:37 +0530

    update user routes in App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,7 @@ import EditTransaction from "./pages/EditTransaction";
 import DeleteTransaction from "./pages/DeleteTransaction";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import LoginRegister from "./pages/LoginRegister";
 function App() {
   return (
     <AuthProvider>
@@ -16,8 +15,7 @@ function App() {
           <Route path="/add" element={<ProtectedRoute><AddTransaction /></ProtectedRoute>} />
           <Route path="/:id/edit" element={<ProtectedRoute><EditTransaction /></ProtectedRoute>} />
           <Route path="/:id/delete" element={<ProtectedRoute><DeleteTransaction /></ProtectedRoute>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<LoginRegister />} />
         </Routes>
       </Router>
     </AuthProvider>
